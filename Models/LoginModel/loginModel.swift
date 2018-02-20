940c924143d4b0ec44f2d240655da52c343af9b5
Abdelrahman Ahmed Shawky <aaaeeejjj1@gmail.com>
2018-02-20 21:19:43 +0200

    My message

---
+++ Models/LoginModel/loginModel.swift
@@ -0,0 +1,13 @@
+//
+//  loginModel.swift
+//  doctor
+//
+//  Created by Abdelrahman Ahmed Shawky on 2/20/18.
+//  Copyright © 2018 andrew. All rights reserved.
+//
+
+import UIKit
+
+class loginModel: NSObject {
+
+}
